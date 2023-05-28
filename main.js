f67ea8f1349e387ff8d872761c669ca81401131a
nanayou0621 <132759432+nanayou0621@users.noreply.github.com>
2023-05-28 15:31:46 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const ice =document.getElementById('ice');
+
+ ice.addEventListener('click',() => {
+
+   ice.style.transform = "scale(1.5,1.5)";
+   console.log('OK');
+ });
+
+}
